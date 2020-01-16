29d5cdcc35b3cae189fd5f46abdf4d6ff87e92b4
KCSF_171103 <KCSF_171103@TABLET-7TOABII4>
2020-01-16 10:42:21 +0900

    chapter5

---
+++ Studyweb/src/chapter5/StudentInfoServlet.java
@@ -0,0 +1,60 @@
+package chapter5;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentInfoServlet
+ */
+@WebServlet("/StudentInfoServlet")
+public class StudentInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentInfoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		int studentNo;
+		String studentName;
+		int schoolYear;
+
+		try {
+			studentNo = Integer.parseInt(request.getParameter("studentno"));
+			studentName = request.getParameter("studentname");
+			schoolYear = Integer.parseInt(request.getParameter("schoolyear"));
+			
+		} catch (NumberFormatException e) {
+			getServletContext().getRequestDispatcher("/errorinput.html").forward(request, response);
+			return;
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
