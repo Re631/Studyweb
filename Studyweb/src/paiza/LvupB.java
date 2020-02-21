1bbea12bdcecfcb32e940f668f4f6f994d9410a0
KCSF_171103 <KCSF_171103@TABLET-7TOABII4>
2020-02-21 10:45:23 +0900

    a

--- Studyweb/src/paiza/LvupB.java
+++ Studyweb/src/paiza/LvupB.java
@@ -1,16 +1,43 @@
 package paiza;
 
+import java.util.Scanner;
+
 public class LvupB {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-        int n_list[] = {1, 3, 5, 6, 3, 2, 5, 23, 2};
+        int sekisu = sc.nextInt();
+        int group = sc.nextInt();
+        boolean seki[] = new boolean[sekisu];
+        boolean flag = true;
         int sum = 0;
-
-        // 取得した値を標準出力するために"XXXXXX"をどう書き換える？
-        //System.out.println(n_list);
         
-    for(int i = 0; i < n_list.length; i++) {
-        sum =  sum + n_list[i];
+        for(int i = 0; i <= group; i++){
+            
+            int member = sc.nextInt();
+            int suffix = sc.nextInt();
+            
+            if(member + suffix > sekisu){
+                
+            }
+            
+            //席が空いているか
+            for(int j = suffix; j <= sekisu; j++){
+                if(seki[j] != null){
+                    flag = false;
+                }
+            }
+            
+            //席に座らせる
+            if(flag == true){
+                for(int j = suffix; j <= sekisu; j++){
+                    seki[j] = true;
+                    sum = sum + 1;
+                }
+            }
+            
+        }
+        System.out.println(sum);
+        
     }
-    System.out.println(sum);
-}}
+}
