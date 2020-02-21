1faa791cb90fcafd2e59483e02a31dff89028930
r3a1 <r3a1@KCSF0916.KCS-Learning.local>
2020-02-21 12:31:04 +0900

    tsutsumi

--- Studyweb/src/paiza/C051.java
+++ Studyweb/src/paiza/C051.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class C051 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        
 
+        String slist[] = {sc.nextLine()} ;
+
+        for (int i = 0; i < slist.length; i++) {
+
+        }
     }
 }
