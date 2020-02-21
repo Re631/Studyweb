430934915ceefce414dc1e6123f0f483d05d7e5e
r3a1 <r3a1@KCSF0916.KCS-Learning.local>
2020-02-21 12:27:37 +0900

    tsutsumi

---
+++ Studyweb/src/paiza/C051.java
@@ -0,0 +1,12 @@
+package paiza;
+
+import java.util.Scanner;
+
+public class C051 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+
+    }
+}
--- Studyweb/src/paiza/LvupB.java
+++ Studyweb/src/paiza/LvupB.java
@@ -5,39 +5,51 @@ import java.util.Scanner;
 public class LvupB {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int sekisu = sc.nextInt();
         int group = sc.nextInt();
-        boolean seki[] = new boolean[sekisu];
-        boolean flag = true;
+        int seki[] = new int[sekisu + 1];
+
         int sum = 0;
-        
-        for(int i = 0; i <= group; i++){
-            
+
+        for(int i = 1; i <= group; i++){
+        	boolean flag = true;
+
             int member = sc.nextInt();
             int suffix = sc.nextInt();
-            
+            /*
             if(member + suffix > sekisu){
-                
+				sekisu - (member + suffix + j);
             }
-            
+            */
+
             //席が空いているか
-            for(int j = suffix; j <= sekisu; j++){
-                if(seki[j] != null){
+            for(int j = 0; j < member; j++){
+
+            	if(suffix + j > sekisu){
+            		if(seki[(suffix + j) - sekisu] != 0) {
+            			flag = false;
+            		}
+                }else if(seki[suffix + j] != 0){
                     flag = false;
                 }
             }
-            
+
             //席に座らせる
             if(flag == true){
-                for(int j = suffix; j <= sekisu; j++){
-                    seki[j] = true;
+                for(int j = 0; j < member; j++){
+                	if(suffix + j > sekisu){
+                		seki[(suffix + j) - sekisu] = 1;
+                	}else {
+                		seki[suffix + j] = 1;
+                	}
                     sum = sum + 1;
                 }
+
             }
-            
+
         }
         System.out.println(sum);
-        
+
     }
 }
